768c97032a761a394f977a41db4bdac05071093e
HADYZELLEYA\hadi_ <hadi_scorpion95@hotmail.com>
2025-07-07 15:01:34 +0300

    created the navbar and some of the hero section with animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 export default function App() {
   return (
     <>
-      <div className="flex-center">App</div>
-      <nav className="">
-        <p>hello world</p>
-      </nav>
+      <main>
+        <Navbar />
+        <Hero />
+        <div className="h-dvh bg-black" />
+      </main>
     </>
   );
 }
